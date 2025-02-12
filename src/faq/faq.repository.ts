2e549ac6c2a66cc19f1cb655dc78a222d7d75e5a
twh <owootak@naver.com>
2025-02-12 23:26:57 +0900

    ♻️ [Refactor] : Move orderBy dto

--- src/faq/faq.repository.ts
+++ src/faq/faq.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/common/prismaClient';
 import { CreateFaqDto } from './dto/create.faq.dto';
 import { UpdateFaqDto } from './dto/update.faq.dto';
-import { OrderByQueryDto } from './dto/orderBy.dto';
+import { OrderByQueryDto } from '../common/dto/orderBy.dto';
 @Injectable()
 export class FaqRepository {
   constructor(private readonly prismaClient: PrismaService) {}
