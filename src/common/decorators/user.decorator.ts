c8e48132bd97e0a5f85f08b4c384e207f732cf96
twh <owootak@naver.com>
2025-02-13 19:07:51 +0900

    💡 [Comment] : UPDATE User decorator comment

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -3,8 +3,8 @@ import { ExecutionContext } from '@nestjs/common';
 
 export const User = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    const user = request.user;
-    return data ? user?.[data] : user;
+    const request = ctx.switchToHttp().getRequest(); // 요청 객체 가져오기
+    const user = request.user; // 요청 객체에서 user 속성 가져오기
+    return data ? user?.[data] : user; // 데코레이터에 전달된 데이터가 있으면 해당 데이터 반환, 없으면 전체 user 객체 반환
   },
 );
