5b1d4eb4e7ea108bb2752943a2b05189a09cc1d4
twh <owootak@naver.com>
2025-02-13 22:15:12 +0900

    ✨ [New Feature] : CREATE global error filter

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
 import { PrismaService } from './prismaClient';
+
 @Module({
   controllers: [CommonController],
   providers: [CommonService, PrismaService],
---
+++ src/common/filters/httpException.filter.ts
@@ -0,0 +1,41 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+    }
+
+    console.log({
+      path: request.path,
+      method: request.method,
+      error: {
+        name: exception.name,
+        message: exception.message,
+        statusCode: status,
+      },
+      date: new Date(),
+    });
+
+    response.status(status).json({
+      path: request.path,
+      method: request.method,
+      status,
+      message: exception.message || 'Internal Server Error',
+      date: new Date(),
+    });
+  }
+}
