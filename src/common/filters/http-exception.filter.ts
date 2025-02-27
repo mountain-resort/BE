6063efe97b959a9dd6ae065a9a6ed9bbfb83726a
amberim <im.amberim@gmail.com>
2025-02-27 20:38:44 +0900

    :recycle: [Refactor] add error exception for findUniqueOrThrow on global error filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -6,6 +6,7 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { Prisma } from '@prisma/client';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
@@ -15,8 +16,16 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest<Request>();
 
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = exception.message || 'Internal Server Error';
+
     if (exception instanceof HttpException) {
       status = exception.getStatus();
+    } else if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+      //prisma findUniqueOrThrow 했을때 던지는 오류
+      if (exception.code === 'P2025') {
+        status = HttpStatus.NOT_FOUND;
+        message = 'id not found';
+      }
     }
 
     console.log({
@@ -30,7 +39,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
       path: request.path,
       method: request.method,
       status,
-      message: exception.message || 'Internal Server Error',
+      message,
       date: new Date(),
     });
   }
