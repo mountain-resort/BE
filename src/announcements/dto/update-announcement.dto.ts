9e035d16f48720924a41ccfb8b7199fea4af0e8c
twh <owootak@naver.com>
2025-03-03 21:42:46 +0900

    ✨ [Feat] Create Announcements module

---
+++ src/announcements/dto/update-announcement.dto.ts
@@ -0,0 +1,3 @@
+import { CreateAnnouncementDto } from './create-announcement.dto';
+import { PartialType } from '@nestjs/mapped-types';
+export class UpdateAnnouncementDto extends PartialType(CreateAnnouncementDto) {}
